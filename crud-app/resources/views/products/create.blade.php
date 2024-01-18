c9147d6749fb32f352879bebcc88879428a17af9
Hertzy <helter.xavier@hotmail.com>
2024-01-18 09:34:17 -0300

    18-08-2023

--- crud-app/resources/views/products/create.blade.php
+++ crud-app/resources/views/products/create.blade.php
@@ -19,7 +19,7 @@
                                 {{ __("Cadastre as informações de um novo produto") }}
                             </p>
                         </header>
-                        <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+                        <form method="post" action="{{ route('create') }}" class="mt-6 space-y-6">
                         @csrf
                         @method('patch')
 
